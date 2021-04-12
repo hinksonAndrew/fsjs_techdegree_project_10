186f1bb86950e64646cda915363931f69581e008
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2021-04-12 11:51:59 -0500

    added css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './styles/reset.css';
+import './styles/global.css';
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/courses')
+      .then(res => res.json())
+      .then(data => setCourses(data))
+      .catch(error => console.log('Error fetching and parsing data', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {courses.map(course => (
+        <li>{course.title}</li>
+      ))}
+      </ul>
     </div>
   );
 }
