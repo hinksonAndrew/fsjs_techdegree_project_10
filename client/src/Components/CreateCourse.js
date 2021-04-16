df4d8b18f78af90a97d039dc07c7fed5684794d4
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2021-04-16 18:19:12 -0500

    started work on CreateCourse.js

--- client/src/Components/CreateCourse.js
+++ client/src/Components/CreateCourse.js
@@ -1,52 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Form from './Form';
 
-const CreateCourse = (props) => {
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
+export default class CreateCourse extends Component {
+  state = {
+    courseTitle: '',
+    courseAuthor: '',
+    courseDescription: '',
+    estimatedTime: '',
+    materialsNeeded: ''
   }
+  render() {
+    const {
+      courseTitle,
+      courseAuthor,
+      courseDescription,
+      estimatedTime,
+      materialsNeeded
+    } = this.state;
+  
+    return (
+      <div className="wrap">
+        <h2>Create Course</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="main--flex">
+            <div>
+              <label htmlFor="courseTitle">Course Title</label>
+              <input id="courseTitle" name="courseTitle" type="text" defaultValue="" />
 
-  const handleCancel = (e) => {
-    e.preventDefault();
-    props.history.push('/');
-  }
+              <label htmlFor="courseAuthor">Course Author</label>
+              <input id="courseAuthor" name="courseAuthor" type="text" defaultValue="Joe Smith" />
 
-  return (
-    <div className="wrap">
-      <h2>Create Course</h2>
-      <div className="validation--errors">
-        <h3>Validation Errors</h3>
-        <ul>
-          <li>Please provide a value for "Title"</li>
-          <li>Please provide a value for "Description"</li>
-        </ul>
+              <label htmlFor="courseDescription">Course Description</label>
+              <textarea id="courseDescription" name="courseDescription"></textarea>
+            </div>
+            <div>
+              <label htmlFor="estimatedTime">Estimated Time</label>
+              <input id="estimatedTime" name="estimatedTime" type="text" defaultValue="" />
+
+              <label htmlFor="materialsNeeded">Materials Needed</label>
+              <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+            </div>
+          </div>
+          <button className="button" type="submit">Create Course</button>
+          <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="main--flex">
-          <div>
-            <label htmlFor="courseTitle">Course Title</label>
-            <input id="courseTitle" name="courseTitle" type="text" defaultValue="" />
-
-            <label htmlFor="courseAuthor">Course Author</label>
-            <input id="courseAuthor" name="courseAuthor" type="text" defaultValue="Joe Smith" />
-
-            <label htmlFor="courseDescription">Course Description</label>
-            <textarea id="courseDescription" name="courseDescription"></textarea>
-          </div>
-          <div>
-            <label htmlFor="estimatedTime">Estimated Time</label>
-            <input id="estimatedTime" name="estimatedTime" type="text" defaultValue="" />
-
-            <label htmlFor="materialsNeeded">Materials Needed</label>
-            <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
-          </div>
-        </div>
-        <button className="button" type="submit">Create Course</button>
-        <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
-      </form>
-    </div>
-  );
+    );
+  };
 };
-
-export default CreateCourse;
